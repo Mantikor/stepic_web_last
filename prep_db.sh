0d932a22368e810fe563e01049a24019eeccd7b6
Mantikor <mantikor@tut.by>
2016-03-13 15:13:08 +0000

    cosmetic

--- prep_db.sh
+++ prep_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo service mysql start
-mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box'"
-mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost'"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u box -p box -e "create database ask"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost'"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -ubox -pbox -e "create database ask"
 
 python ./ask/manage.py syncdb
